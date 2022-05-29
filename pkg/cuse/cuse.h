3f57c9f79e2d6ee63bdd1e60a3a7b66de35c52cc
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-05-29 20:33:34 +0200

    refactor: Use libffcall instead of GNU nested function extension for stateful callbacks

--- pkg/cuse/cuse.h
+++ pkg/cuse/cuse.h
@@ -6,5 +6,5 @@ typedef struct fuse_conn_info fuse_conn_info;
 typedef struct fuse_file_info fuse_file_info;
 typedef struct fuse_pollhandle fuse_pollhandle;
 
-int wbcuse_start(void *device, unsigned int major, unsigned int minor,
+int gocuse_start(void *device, unsigned int major, unsigned int minor,
                  char *name, int argc, char **argv);
